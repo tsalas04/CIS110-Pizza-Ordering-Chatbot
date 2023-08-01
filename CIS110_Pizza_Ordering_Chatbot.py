af649dffb37cb5b19b4a2c44782fd9bd7a8c6142
username <useremail>
2023-07-31 18:04:16 -0700

    Added Data Validation While loops.

--- CIS110_Pizza_Ordering_Chatbot.py
+++ CIS110_Pizza_Ordering_Chatbot.py
@@ -1,16 +1,33 @@
 print("Hello, my name is Alex your virtual assistant.  I will help you order a pizza!")
 print("I am going to ask you a few questions.  After typing an answer, press enter.")
 userName = input("\nEnter your name:  ")
+while len(userName) == 0:
+    userName = input("Name cannot be blank! Please enter your name: ")
+
 if userName.lower() == "professor quigley":
     print(f"\nMy creator, {userName}. Pleasure to serve you!")
 else:
     print(f"\nHello, {userName}. Nice to meet you!")
-size = input("\nWhat size do you want?  Enter small, medium, or large:  ")
-flavor = input("\nenter the flavor of pizza:  ")
+size = input("\nWhat size do you want? Enter small, medium, or large:  ")
+while len(size) == 0:
+    size = input("Invalid value! Please enter small, medium, or large:  ")
+
+flavor = input("\Enter the flavor of pizza  ")
+while len(flavor) == 0:
+    flavor = input("Flavor cannot be blank! Please enter a flavor:  ")
+
 crustType = input("\nWhat type of crust do you want:  ")
+while len(crustType) == 0:
+    crustType = input("Crust type cannot be blank! Please enter crust type:  ")
 quantity = input("\nHow many of these do you want to order?  Enter a numeric value:  ")
+
+while not quantity.isdigit():
+    quantity = input("nValue not recognized. Please enter a numeric value:  ")
+
 quantity = int(quantity)
 method = input("\nIs this carry out or delivery:  ")
+while method not in ["carry out", "delivery"]:
+    method = input("Invalid value! Please enter carry out or delivery:  ")
 if method.lower() == "delivery":
     deliveryFee = 5
 else:
@@ -22,7 +39,9 @@ elif size.lower() == "medium":
     pizzaCost = 14.99
 elif size.lower() == "large":
     pizzaCost = 17.99
+
 total = (pizzaCost * quantity) * salesTax + deliveryFee
+
 print("_" * 10)
 print(f"Thank you, {userName}, for your order")
 print(f"Your {quantity} {size} {flavor} pizza(s) with {crustType} crust costs ${total:,.2f}.")
